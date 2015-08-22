171dd9db37ed6ac09c418d475e00a6d11df2c25d
Ashok.Dudhade <ashok.dudhade@asurion.com>
2015-08-22 13:36:03 -0500

    Added some more tests

--- test/superBustTest.js
+++ test/superBustTest.js
@@ -2,6 +2,7 @@ var assert = require("assert");
 var superBust = require("../lib/superBust.js");
 
 describe('Super cache bust test', function() {
+
     describe('superBust with invalid param', function() {
         it('Should return nothing with invalid paramter', function() {
             var content = superBust.applyVersion();
@@ -10,16 +11,16 @@ describe('Super cache bust test', function() {
     });
 
     describe('superBust with valid param', function() {
-        it('Should return content with scripts and links versioned MD5', function() {
+        it('Should return content with scripts and links versioned MD5 from source directory', function() {
             var content = "<html><script src='data/some.js?nnn=nn&version=kk'></script><script src='data/someOther.js'></script><link rel='stylesheet' href = 'data/somecss.css'></link></html>";
 
-            var regex = /src='data\/someOther.js'/;
+            var regex = /version=kk'/;
             var isVersionExists = !regex.test(content);
 
-            var content = superBust.applyVersion({
+            content = superBust.applyVersion({
                 content: content,
                 sourceDir: "/test",
-                versionType : "MD5"
+                versionType: "MD5"
             });
             assert.equal(isVersionExists, false);
             assert.equal(/'data\/someOther.js\?version([^$]*)'/.test(content), true);
@@ -28,16 +29,51 @@ describe('Super cache bust test', function() {
 
         });
 
+        it('Should return content with scripts and links versioned MD5 from current directory', function() {
+            var content = "<html><script src='test/data/some.js?nnn=nn&version=kk'></script><script src='test/data/someOther.js'></script><link rel='stylesheet' href = 'test/data/somecss.css'></link></html>";
+
+            var regex = /version=kk'/;
+            var isVersionExists = !regex.test(content);
+
+            content = superBust.applyVersion({
+                content: content,
+                versionType: "MD5"
+            });
+
+            assert.equal(isVersionExists, false);
+            assert.equal(/data\/someOther.js\?version([^$]*)'/.test(content), true);
+            assert.equal(/data\/somecss.css\?version([^$]*)'/.test(content), true);
+            assert.equal(/version=kk'/.test(content), false);
+
+        });
+
+        it('Should return same content if no change in static content files', function() {
+            var content = "<html><script src='test/data/some.js?nnn=nn&version=kk'></script><script src='test/data/someOther.js'></script><link rel='stylesheet' href = 'test/data/somecss.css'></link></html>";
+
+            var firstContent = superBust.applyVersion({
+                content: content,
+                versionType: "MD5"
+            });
+
+            var secondContent = superBust.applyVersion({
+                content: content,
+                versionType: "MD5"
+            });
+
+            assert.equal(firstContent, secondContent);
+
+        });
+
         it('Should return content with scripts and links versioned timestamp', function() {
             var content = "<html><script src='data/some.js?nnn=nn&version=kk'></script><script src='data/someOther.js'></script><link rel='stylesheet' href = 'data/somecss.css'></link></html>";
 
-            var regex = /src='data\/someOther.js'/;
+            var regex = /version=kk'/;
             var isVersionExists = !regex.test(content);
 
-            var content = superBust.applyVersion({
+            content = superBust.applyVersion({
                 content: content,
                 sourceDir: "/test",
-                versionType : "MD5"
+                versionType: "MD5"
             });
             assert.equal(isVersionExists, false);
             assert.equal(/'data\/someOther.js\?version([^$]*)'/.test(content), true);
@@ -49,13 +85,13 @@ describe('Super cache bust test', function() {
         it('Should return content with scripts and links versioned custom', function() {
             var content = "<html><script src='data/some.js?nnn=nn&version=kk'></script><script src='data/someOther.js'></script><link rel='stylesheet' href = 'data/somecss.css'></link></html>";
 
-            var regex = /src='data\/someOther.js'/;
+            var regex = /version=kk'/;
             var isVersionExists = !regex.test(content);
 
-            var content = superBust.applyVersion({
+            content = superBust.applyVersion({
                 content: content,
                 sourceDir: "/test",
-                versionType : "custom",
+                versionType: "custom",
                 customVersion: "0000"
             });
             assert.equal(isVersionExists, false);
@@ -69,14 +105,14 @@ describe('Super cache bust test', function() {
         it('Should return content with scripts and links versioned custom function', function() {
             var content = "<html><script src='data/some.js?nnn=nn&version=kk'></script><script src='data/someOther.js'></script><link rel='stylesheet' href = 'data/somecss.css'></link></html>";
 
-            var regex = /src='data\/someOther.js'/;
+            var regex = /version=kk'/;
             var isVersionExists = !regex.test(content);
 
-            var content = superBust.applyVersion({
+            content = superBust.applyVersion({
                 content: content,
                 sourceDir: "/test",
-                versionType : "custom",
-                customVersion: function(){
+                versionType: "custom",
+                customVersion: function() {
                     return "newVersion"
                 }
             });
