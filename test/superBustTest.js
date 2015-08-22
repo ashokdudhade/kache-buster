302b33fb91fd5933cb04fda1beb327c7d69b3799
Ashok.Dudhade <ashok.dudhade@asurion.com>
2015-08-22 13:09:39 -0500

    Added more tests

--- test/superBustTest.js
+++ test/superBustTest.js
@@ -37,11 +37,53 @@ describe('Super cache bust test', function() {
             var content = superBust.applyVersion({
                 content: content,
                 sourceDir: "/test",
-                versionType : "timestamp"
+                versionType : "MD5"
             });
             assert.equal(isVersionExists, false);
             assert.equal(/'data\/someOther.js\?version([^$]*)'/.test(content), true);
             assert.equal(/'data\/somecss.css\?version([^$]*)'/.test(content), true);
+            assert.equal(/version=kk'/.test(content), false);
+
+        });
+
+        it('Should return content with scripts and links versioned custom', function() {
+            var content = "<html><script src='data/some.js?nnn=nn&version=kk'></script><script src='data/someOther.js'></script><link rel='stylesheet' href = 'data/somecss.css'></link></html>";
+
+            var regex = /src='data\/someOther.js'/;
+            var isVersionExists = !regex.test(content);
+
+            var content = superBust.applyVersion({
+                content: content,
+                sourceDir: "/test",
+                versionType : "custom",
+                customVersion: "0000"
+            });
+            assert.equal(isVersionExists, false);
+            assert.equal(/'data\/someOther.js\?version=0000'/.test(content), true);
+            assert.equal(/'data\/somecss.css\?version=0000'/.test(content), true);
+            assert.equal(/'data\/some.js\?nnn=nn&version=0000'/.test(content), true);
+            assert.equal(/version=kk'/.test(content), false);
+
+        });
+
+        it('Should return content with scripts and links versioned custom function', function() {
+            var content = "<html><script src='data/some.js?nnn=nn&version=kk'></script><script src='data/someOther.js'></script><link rel='stylesheet' href = 'data/somecss.css'></link></html>";
+
+            var regex = /src='data\/someOther.js'/;
+            var isVersionExists = !regex.test(content);
+
+            var content = superBust.applyVersion({
+                content: content,
+                sourceDir: "/test",
+                versionType : "custom",
+                customVersion: function(){
+                    return "newVersion"
+                }
+            });
+            assert.equal(isVersionExists, false);
+            assert.equal(/'data\/someOther.js\?version=newVersion'/.test(content), true);
+            assert.equal(/'data\/somecss.css\?version=newVersion'/.test(content), true);
+            assert.equal(/'data\/some.js\?nnn=nn&version=newVersion'/.test(content), true);
             assert.equal(/version=kk'/.test(content), false);
 
         });
