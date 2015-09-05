85d823cae82c4e54747b4bbf9dcfe4c05c16b7a3
Ashok.Dudhade <ashok.dudhade@asurion.com>
2015-09-05 10:38:20 -0500

    Added some more gulp task for testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
 var gulp = require('gulp');
 var kacheBuster = require('./index.js');
-gulp.task('default', function() {
+gulp.task('default', ['MD5','timestamp', 'custom']);
+
+gulp.task('MD5', function() {
     return gulp.src('test/data/*.html')
         .pipe(kacheBuster({ versionType: "MD5", sourceDir : "/test"}))
-        .pipe(gulp.dest('./tmp'));
+        .pipe(gulp.dest('./tmp_MD5'));
+});
+
+
+gulp.task('timestamp', function() {
+    return gulp.src('test/data/*.html')
+        .pipe(kacheBuster({ versionType: "timestamp"}))
+        .pipe(gulp.dest('./tmp_timestamp'));
+});
+
+gulp.task('custom', function() {
+    return gulp.src('test/data/*.html')
+        .pipe(kacheBuster({ versionType: "custom", customVersion: "9000"}))
+        .pipe(gulp.dest('./tmp_custom'));
 });
